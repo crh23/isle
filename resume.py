c742f3ae38420ccd5b2d18e2a078e446cd7d0239
KloskaT <tpk116@ic.ac.uk>
2019-07-11 11:20:47 +0100

    Save state now includes events. Removed abce from resume.py and now works, including network visualisation.

--- resume.py
+++ resume.py
@@ -17,19 +17,18 @@ override_no_riskmodels = False
 
 # use argparse to handle command line arguments
 parser = argparse.ArgumentParser(description='Model the Insurance sector')
-parser.add_argument("--abce", action="store_true", help="use abce")
 parser.add_argument("--oneriskmodel", action="store_true", help="allow overriding the number of riskmodels from the standard config (with 1)")
 parser.add_argument("--riskmodels", type=int, choices=[1,2,3,4], help="allow overriding the number of riskmodels from standard config (with 1 or other numbers)")
 parser.add_argument("--replicid", type=int, help="if replication ID is given, pass this to the simulation so that the risk profile can be restored")
 parser.add_argument("--replicating", action="store_true", help="if this is a simulation run designed to replicate another, override the config file parameter")
 parser.add_argument("--randomseed", type=float, help="allow setting of numpy random seed")
 parser.add_argument("--foreground", action="store_true", help="force foreground runs even if replication ID is given (which defaults to background runs)")
+parser.add_argument("--shownetwork", action="store_true", help="show reinsurance relations as network")
 parser.add_argument("-p", "--showprogress", action="store_true", help="show timesteps")
 parser.add_argument("-v", "--verbose", action="store_true", help="more detailed output")
+parser.add_argument("--save_iterations", type=int, help="number of iterations to iterate before saving world state")
 args = parser.parse_args()
 
-if args.abce:
-    isleconfig.use_abce = True
 if args.oneriskmodel:
     isleconfig.oneriskmodel = True
     override_no_riskmodels = 1
@@ -48,41 +47,25 @@ else:
     seed = np.random.randint(0, 2 ** 31 - 1)
 if args.foreground:
     isleconfig.force_foreground = True
+if args.shownetwork:
+    isleconfig.show_network = True
 if args.showprogress:
     isleconfig.showprogress = True
 if args.verbose:
     isleconfig.verbose = True
+if args.save_iterations:
+    save_iter = args.save_iterations
+else:
+    save_iter = 200
 
-# import isle and abce modules
-if isleconfig.use_abce:
-    #print("Importing abce")
-    import abce
-    from abce import gui
-
+# import isle modules
 from insurancesimulation import InsuranceSimulation
 from insurancefirm import InsuranceFirm
 from riskmodel import RiskModel
 from reinsurancefirm import ReinsuranceFirm
 
-# create conditional decorator
-def conditionally(decorator_function, condition):
-    def wrapper(target_function):
-        if not condition:
-            return target_function
-        return decorator_function(target_function)
-    return wrapper
-
-# create non-abce placeholder gui decorator
-# TODO: replace this with more elegant solution if possible. Currently required since script will otherwise crash at the conditional decorator below since gui is then undefined
-if not isleconfig.use_abce:
-    def gui(*args, **kwargs):
-        pass
-
 
 # main function
-
-#@gui(simulation_parameters, serve=True)
-@conditionally(gui(simulation_parameters, serve=False), isleconfig.use_abce)
 def main():
     
     with open("data/simulation_save.pkl", "br") as rfile:
@@ -93,56 +76,25 @@ def main():
         random_seed = d["random_seed"]
         time = d["time"]
         simulation_parameters = d["simulation_parameters"]
-    
+        event_schedule = d["event_schedule"]
+        event_damage = d["event_damage"]
+
+    event_schedule_trunc = []
+    event_damage_trunc = []
+    for categ in range(simulation_parameters["no_categories"]):
+        event_schedule_trunc.append([event for event in event_schedule[categ] if event >= time])
+        event_damage_trunc.append(event_damage[categ][-len(event_schedule_trunc[categ]):])
+
     insurancefirms_group = list(simulation.insurancefirms)
     reinsurancefirms_group = list(simulation.reinsurancefirms)
-    
-    #np.random.seed(seed)
+
+    # np.random.seed(seed)
     np.random.set_state(np_seed)
     random.setstate(random_seed)
-    
-    assert not isleconfig.use_abce, "Resuming will not work with abce"
-    ## create simulation and world objects (identical in non-abce mode)
-    #if isleconfig.use_abce:
-    #    simulation = abce.Simulation(processes=1,random_seed = seed)
-    #
-        
-    #simulation_parameters['simulation'] = world = InsuranceSimulation(override_no_riskmodels, replic_ID, simulation_parameters)
-    #
-    #if not isleconfig.use_abce:
-    #    simulation = world
-    #
-    # create agents: insurance firms 
-    #insurancefirms_group = simulation.build_agents(InsuranceFirm,
-    #                                         'insurancefirm',
-    #                                         parameters=simulation_parameters,
-    #                                         agent_parameters=world.agent_parameters["insurancefirm"])
-    #
-    #if isleconfig.use_abce:
-    #    insurancefirm_pointers = insurancefirms_group.get_pointer()
-    #else:
-    #    insurancefirm_pointers = insurancefirms_group
-    #world.accept_agents("insurancefirm", insurancefirm_pointers, insurancefirms_group)
-    #
-    # create agents: reinsurance firms 
-    #reinsurancefirms_group = simulation.build_agents(ReinsuranceFirm,
-    #                                           'reinsurance',
-    #                                           parameters=simulation_parameters,
-    #                                           agent_parameters=world.agent_parameters["reinsurance"])
-    #if isleconfig.use_abce:
-    #    reinsurancefirm_pointers = reinsurancefirms_group.get_pointer()
-    #else:
-    #    reinsurancefirm_pointers = reinsurancefirms_group
-    #world.accept_agents("reinsurance", reinsurancefirm_pointers, reinsurancefirms_group)
-    #
-    
-    # time iteration
+
     for t in range(time, simulation_parameters["max_time"]):
         
-        # abce time step
-        simulation.advance_round(t)
-        
-        # create new agents             # TODO: write method for this; this code block is executed almost identically 4 times
+        # create new agents        # TODO: write method for this; this code block is executed almost identically 4 times
         if world.insurance_firm_market_entry(agent_type="InsuranceFirm"):
             parameters = [np.random.choice(world.agent_parameters["insurancefirm"])]
             parameters[0]["id"] = world.get_unique_insurer_id()
@@ -151,46 +103,28 @@ def main():
                                              parameters=simulation_parameters,
                                              agent_parameters=parameters)
             insurancefirms_group += new_insurance_firm
-            if isleconfig.use_abce:
-                # TODO: fix abce
-                # may fail in abce because addressing individual agents may not be allowed
-                # may also fail because agent methods may not be callable directly
-                new_insurancefirm_pointer = [new_insurance_firm[0].get_pointer()]        # index 0 because this is a list with just 1 object
-            else:
-                new_insurancefirm_pointer = new_insurance_firm
+            new_insurancefirm_pointer = new_insurance_firm
             world.accept_agents("insurancefirm", new_insurancefirm_pointer, new_insurance_firm, time=t)
         
         if world.insurance_firm_market_entry(agent_type="ReinsuranceFirm"):
-            parameters = [np.random.choice(world.agent_parameters["reinsurance"])]
+            parameters = [np.random.choice(world.agent_parameters["reinsurancefirm"])]
             parameters[0]["id"] = world.get_unique_reinsurer_id()
             new_reinsurance_firm = simulation.build_agents(ReinsuranceFirm,
-                                             'reinsurance',
+                                             'reinsurancefirm',
                                              parameters=simulation_parameters,
                                              agent_parameters=parameters)
             reinsurancefirms_group += new_reinsurance_firm
-            if isleconfig.use_abce:
-                # TODO: fix abce
-                # may fail in abce because addressing individual agents may not be allowed
-                # may also fail because agent methods may not be callable directly
-                new_reinsurancefirm_pointer = [new_reinsurance_firm[0].get_pointer()]        # index 0 because this is a list with just 1 object
-            else:
-                new_reinsurancefirm_pointer = new_reinsurance_firm
-            world.accept_agents("reinsurance", new_reinsurancefirm_pointer, new_reinsurance_firm, time=t)
+            new_reinsurancefirm_pointer = new_reinsurance_firm
+            world.accept_agents("reinsurancefirm", new_reinsurancefirm_pointer, new_reinsurance_firm, time=t)
         
         # iterate simulation
         world.iterate(t)
         
         # log data
-        if isleconfig.use_abce:
-            #insurancefirms.logme()
-            #reinsurancefirms.logme()
-            insurancefirms_group.agg_log(variables=['cash', 'operational'], len=['underwritten_contracts'])
-            #reinsurancefirms_group.agg_log(variables=['cash'])
-        else:
-            world.save_data()
+        world.save_data()
         
         if t > 0 and t//50 == t/50:
-            save_simulation(t, simulation, simulation_parameters, exit_now=False)
+            save_simulation(t, simulation, simulation_parameters, event_schedule, event_damage, exit_now=False)
         #print("here")
     
     # finish simulation, write logs
@@ -198,13 +132,15 @@ def main():
 
 
 # save function
-def save_simulation(t, sim, sim_param, exit_now=False):
+def save_simulation(t, sim, sim_param, event_schedule, event_damage, exit_now=False):
     d = {}
     d["np_seed"] = np.random.get_state()
     d["random_seed"] = random.getstate()
     d["time"] = t
     d["simulation"] = sim
     d["simulation_parameters"] = sim_param
+    d["event_schedule"] = event_schedule
+    d["event_damage"] = event_damage
     with open("data/simulation_resave.pkl", "bw") as wfile:
         pickle.dump(d, wfile, protocol=pickle.HIGHEST_PROTOCOL)
     with open("data/simulation_resave.pkl", "br") as rfile:
@@ -214,7 +150,8 @@ def save_simulation(t, sim, sim_param, exit_now=False):
     print("\nSimulation hash: ",  hashlib.sha512(str(file_contents).encode()).hexdigest())    
     if exit_now:
         exit(0)
-    
+
+
 # main entry point
 if __name__ == "__main__":
     main()
